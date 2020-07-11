17ee9a08780d247bf7064b4350965b889b78702a
Paulo Marques <pepcmarques@gmail.com>
2020-07-11 15:32:38 -0700

    back to begin

--- circularis/base/test/test_simple.py
+++ circularis/base/test/test_simple.py
@@ -1,11 +1,6 @@
-from django.test import Client  # noqa
+from django.test import Client
 
 
-def test_fake():
-    assert 1 == 1
-
-"""
 def test_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200
-"""

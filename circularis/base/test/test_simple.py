19da7f16e4a839ac945ed5fb90f008da09de7b2f
Paulo Marques <pepcmarques@gmail.com>
2020-07-11 19:57:29 -0700

    trying to fix whitenoise

--- circularis/base/test/test_simple.py
+++ circularis/base/test/test_simple.py
@@ -1,6 +1,10 @@
 from django.test import Client
 
 
+def test_to_pass():
+    assert 1 == 1
+
+
 def test_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200

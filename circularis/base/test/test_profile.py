afd49561c1976cf3f8b67d4b23944c5af9fae083
Paulo Marques <pepcmarques@gmail.com>
2020-09-08 15:12:37 -0700

    fixing test

--- circularis/base/test/test_profile.py
+++ circularis/base/test/test_profile.py
@@ -2,15 +2,15 @@ from django.urls import reverse
 
 
 def test_profile_page_no_logged_user(client, user):
-    resp = client.get(reverse("accounts:profile", args=[1]))
+    resp = client.get(reverse("accounts:profile"))
     assert resp.status_code == 302  # redirect
 
 
 def test_profile_page_logged_user(user, logged_user_client):
-    resp = logged_user_client.get(reverse("accounts:profile", args=[user.id]))
+    resp = logged_user_client.get(reverse("accounts:profile"))
     assert resp.status_code == 200
 
 
 def test_change_user_data(user, logged_user_client):
-    resp = logged_user_client.get(reverse("accounts:profile", args=[user.id]))
+    resp = logged_user_client.get(reverse("accounts:profile"))
     assert resp.status_code == 200
